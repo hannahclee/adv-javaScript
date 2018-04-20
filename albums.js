3523aae56ad64edff26c14ba7228c6ca766afef0
Hannah Lee <bananapea@hannahs-mbp.hitronhub.home>
2018-04-20 16:18:20 -0700

    updated albums and troubleshooted delete method

--- albums.js
+++ albums.js
@@ -1,9 +1,9 @@
 let albums = [
 { title : "Blue", artist : "Joni Mitchell", year : "1971" },
-{ title : "Born This Way", artist : "Lady Gaga", year : "2011" },
-{ title : "Mona Bone Jakon", artist : "Cat Stevens", year : "1970" },
-{ title : "Hounds of Love", artist : "Kate Bush", year : "1985" },
-{ title : "Crime of the Century", artist: "Supertramp", year : "1974" }
+{ title : "Lemonade", artist : "Beyonce", year : "2016" },
+{ title : "Aqualung", artist : "Jethro Tull", year : "1970" },
+{ title : "Artpop", artist : "Lady Gaga", year : "2013" },
+{ title : "Help", artist: "The Beatles", year : "1965" }
 ];
 
 exports.getAllAlbums = ()=>{
@@ -11,18 +11,29 @@ exports.getAllAlbums = ()=>{
 };
 
 exports.deleteAlbum = (title)=>{
-    let result = albums.findIndex(function(title){return title});
+        let result = albums.findIndex(
+        (album)=>{
+     title === album.title.toLowerCase()}
+                );
+    console.log(result);
     albums.splice(result, result);
-    return title + ' removed';
-    
 
     
+    if (result < 0){
+        return 'Album titled: ' + title + ' not removed';
+    }
+
+    return 'Album titled: ' + title + ' has been removed';
+
+
 };
+    
+    
 
 exports.getAlbum = (title)=>{
-    let result = albums.find(
-        (album)=>
-     title === album.title.toLowerCase()
+    let result = albums.find( 
+        (album)=>{
+     title === album.title.toLowerCase()}
                 );
     if (result === undefined){
         return 'We do not have that album';
--- index.js
+++ index.js
@@ -2,14 +2,10 @@
 var http = require("http"); 
 var albums = require("./albums.js");
 http.createServer(function(req,res) {
-    console.log('url ' + req.url);
   var parts = req.url.toLowerCase().split('?');
   var path = parts[0];
-if (!path.startsWith('/favicon')){
-    
-    
+if (!path.startsWith('/favicon')){ 
   var query = parts[1];
-    console.log('query ' + query);
   
   switch(path) {
     case '/':
@@ -26,9 +22,7 @@ if (!path.startsWith('/favicon')){
     case '/get':
       var value = query.split('=')[1];
       res.writeHead(200, {'Content-Type': 'text/plain'});
-          console.log(albums.getAlbum(value));
-      res.write("The albums you selected is: " + 
-                JSON.stringify(albums.getAlbum(value)));
+      res.write(JSON.stringify(albums.getAlbum(value)));
       res.end('');
       break;    
       
