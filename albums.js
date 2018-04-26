797260cd80526855f8f29e76ea7ae711b99ff899
hannahclee <33705455+hannahclee@users.noreply.github.com>
2018-04-25 21:49:31 -0700

    Add files via upload

--- albums.js
+++ albums.js
@@ -1,42 +1,36 @@
 let albums = [
-{ title : "Blue", artist : "Joni Mitchell", year : "1971" },
-{ title : "Lemonade", artist : "Beyonce", year : "2016" },
-{ title : "Aqualung", artist : "Jethro Tull", year : "1970" },
-{ title : "Artpop", artist : "Lady Gaga", year : "2013" },
-{ title : "Help", artist: "The Beatles", year : "1965" }
+    { title: "Blue", artist: "Joni Mitchell", year: "1971" },
+    { title: "Lemonade", artist: "Beyonce", year: "2016" },
+    { title: "Aqualung", artist: "Jethro Tull", year: "1970" },
+    { title: "Artpop", artist: "Lady Gaga", year: "2013" },
+    { title: "Help", artist: "The Beatles", year: "1965" }
 ];
 
-exports.getAllAlbums = ()=>{
+exports.getAllAlbums = () => {
     return albums;
 };
 
-exports.deleteAlbum = (title)=>{
-        let result = albums.findIndex(
-        (album)=>
-     title === album.title.toLowerCase()
-                );
-    console.log(result);
-    albums.splice(result, result);
-
-    if (result < 0){
-        return 'Album titled: ' + title + ' not removed';
-    }else{
-        return 'Album titled: ' + title + ' has been removed';
-        
-    };
-
+exports.deleteAlbum = (title) => {
+    let result = albums.findIndex((album) =>{
+        return title.toLowerCase() === album.title.toLowerCase()
+    })
+    if (result > -1) {
+        albums.splice(result, 1);
+        return true;
+    }
+    return false;
 };
-    
-    
 
-exports.getAlbum = (title)=>{
-    let result = albums.find( 
-        (album)=>
-     title === album.title.toLowerCase()
-                );
-    if (result === undefined){
+
+
+exports.getAlbum = (title) => {
+    let result = albums.find(
+        (album) =>
+            title.toLowerCase() === album.title.toLowerCase()
+    );
+    if (result === undefined) {
         return 'We do not have that album';
     }
     return result;
-   
+
 };
--- index.js
+++ index.js
@@ -1,6 +1,73 @@
+'use strict'
+const express = require("express");
+const app = express();
+const albums = require("./albums.js");
+app.set('port', /*process.env.PORT || */ 3000);
+app.use(express.static(__dirname + '/public')); //set location for static files
+app.use(require("body-parser").urlencoded({ extended: true })); //parse form submissions
 
-var http = require("http"); 
-var albums = require("./albums.js");
+let handlebars = require("express-handlebars");
+app.engine("html", handlebars({ extname: 'html' }));
+app.set("view engine", "html");
+
+//send static file as response
+app.get('/', (req, res) => {
+    res.type('text/html');
+    res.render('home', { albums: albums.getAllAlbums() });
+});
+
+//send plain text response
+app.get('/about', (req, res) => {
+    res.type('text/html');
+    res.send('About Page');
+});
+
+//GET handler
+app.get('/delete', (req, res) => {
+    let result = albums.deleteAlbum(req.query.album);
+    let message = "deleted"
+    if(!result){
+        message = "not deleted"
+    };
+    res.render('delete', { 
+        title: req.query.album, 
+        message: message });
+});
+
+app.get('/detail', (req, res) => {
+    console.log(req.query)
+    var found = albums.getAlbum(req.query.album);
+    res.render("detail", { title: req.query.album, 
+        result: found, 
+     });
+});
+
+//POST handler
+app.post('/detail', (req, res) => {
+    let found = albums.getAlbum(req.body.album);
+    res.render("detail",
+        {
+            title: req.body.album,
+            result: found
+        });
+});
+
+//define 404 handler
+app.use((req, res) => {
+    res.type('text/plain');
+    res.status(404);
+    res.send('404 - Not found');
+});
+
+
+app.listen(app.get('port'), () => {
+    console.log('Express started');
+});
+
+
+
+/*var http = require("http"); 
+
 http.createServer(function(req,res) {
   var parts = req.url.toLowerCase().split('?');
   var path = parts[0];
@@ -40,3 +107,4 @@ if (!path.startsWith('/favicon')){
     }
 }
 }).listen(process.env.PORT || 3000);
+*/
